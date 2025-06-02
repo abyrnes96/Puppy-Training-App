d7108a46dc17e5e8f568a1417d8212c6b255d421
Ashley N Byrnes <ashley.byrnes@ey.com>
2025-06-02 14:37:19 -0400

    Add Track Your Progress page and Obedience section

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get("/potty", (req, res) => res.render("potty"));
 app.get("/commands", (req, res) => res.render("commands"));
 app.get("/social", (req, res) => res.render("social"));
 app.get("/schedule", (req, res) => res.render("schedule"));
+app.get("/commands/come", (req, res) => res.render("commands_come"));
+app.get("/commands/place", (req, res) => res.render("commands_place"));
+app.get("/commands/leave-it", (req, res) => res.render("commands_leave_it"));
+app.get("/commands/safe", (req, res) => res.render("commands_safe"));
+app.get("/obedience", (req, res) => res.render("obedience"));
+app.get("/obedience/impulse", (req, res) => res.render("obedience_impulse"));
+app.get("/obedience/biting", (req, res) => res.render("obedience_biting"));
+app.get("/obedience/jumping", (req, res) => res.render("obedience_jumping"));
+app.get("/progress", (req, res) => res.render("progress"));
+
 
 
 app.listen(port, () => {
